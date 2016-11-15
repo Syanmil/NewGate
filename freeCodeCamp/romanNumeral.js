dbb2b32aacc3e2d4f23a912fdd65f6de623992f5
Syanmil <syanmil@yahoo.com>
2016-11-15 16:18:39 +0700

    finishing roman numeral, stuck for 30min at end point

--- freeCodeCamp/romanNumeral.js
+++ freeCodeCamp/romanNumeral.js
@@ -1,10 +1,19 @@
+var angka = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000]
+var roman = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"]
+
 function convertToRoman(num) {
-  var angka = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000]
-  var roman = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"]
- return
+  var hasil = []
+  for (var i = angka.length; i > -1 ; i--){
+    if (num >= angka[i]){
+      hasil.push(roman[i]);
+      num -= angka[i];
+      i += 1
+    }
+  }
+ return hasil.join("")
 }
 
 console.log(convertToRoman(36));
 
 console.log(convertToRoman(3999))
-console.log(convertToRoman(3999)) === "MMMCMXCIX");
+console.log(convertToRoman(3999) === "MMMCMXCIX");
