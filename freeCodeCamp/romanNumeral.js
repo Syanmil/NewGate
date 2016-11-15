d3dc1e9674a6204e3caa85cff5c4c97c8f8f6b92
Syanmil <syanmil@yahoo.com>
2016-11-15 15:49:18 +0700

    Stating angka and roman in array

--- freeCodeCamp/romanNumeral.js
+++ freeCodeCamp/romanNumeral.js
@@ -1,8 +1,10 @@
 function convertToRoman(num) {
- return num;
+  var angka = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000]
+  var roman = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"]
+ return
 }
 
-convertToRoman(36);
+console.log(convertToRoman(36));
 
 console.log(convertToRoman(3999))
 console.log(convertToRoman(3999)) === "MMMCMXCIX");
