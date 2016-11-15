559891e2733acac28bc1640fdd31069d18a66451
Syanmil <syanmil@yahoo.com>
2016-11-15 15:13:22 +0700

    add easy testing for modal

--- freeCodeCamp/DiffTwoArrays.js
+++ freeCodeCamp/DiffTwoArrays.js
@@ -5,6 +5,7 @@ function diffArray(arr1, arr2) {
   return newArr;
 }
 
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]) === [4]);
 console.log(diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]) === ["pink wool"]);
 console.log(diffArray(["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]) === ["diorite", "pink woll"]);
 console.log(diffArray(["andesite", "grass", "dirt", "dead shrub"], ["andesite", "grass", "dirt", "dead shrub"]) === []);
