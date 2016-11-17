ab2787a86de81735179c9f68c74a90132a45bdba
Syanmil <syanmil@yahoo.com>
2016-11-17 14:19:58 +0700

    almost done for all test still wrong at missing first item in list

--- freeCodeCamp/DiffTwoArrays.js
+++ freeCodeCamp/DiffTwoArrays.js
@@ -8,22 +8,23 @@ function diffArray(arr1, arr2) {
     arr1 = banyak;
   }
   for (var i = 0; i < arr1.length; i++){
-    var pow = arr1.indexOf(arr2[i]);
-    // console.log(pow);
-    console.log(arr2[i]);
-    if (pow != -1){
-      arr1.splice(pow, 1);
-      arr2.splice(pow, 1);
+    var pow1 = arr1.indexOf(arr2[i]);
+    var pow2 = arr2.indexOf(arr1[i]);
+    if (pow1 != -1){
+      arr1.splice(pow1, 1);
+      arr2.splice(pow2, 1);
       i--
     }
   }
+  console.log(arr1);
+  console.log(arr2);
   return arr1.concat(arr2);
 }
 
-console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]))
-// console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]) === [4]);
-// console.log(diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]) === ["pink wool"]);
-// console.log(diffArray(["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]) === ["diorite", "pink woll"]);
-// console.log(diffArray(["andesite", "grass", "dirt", "dead shrub"], ["andesite", "grass", "dirt", "dead shrub"]) === []);
-// console.log(diffArray([1, "calf", 3, "piglet"], [1, "calf", 3, 4]) === ["piglet", 4]);
-// console.log(diffArray([1, "calf", 3, "piglet"], [7, "filly"]) === [1, "calf", 3, "piglet", 7, "filly"]);
+//console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]))
+// console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5])) // [4]);
+// console.log(diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]))// === ["pink wool"]);
+console.log(diffArray(["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]))// === ["diorite", "pink woll"]);
+// console.log(diffArray(["andesite", "grass", "dirt", "dead shrub"], ["andesite", "grass", "dirt", "dead shrub"]))// === []);
+// console.log(diffArray([1, "calf", 3, "piglet"], [1, "calf", 3, 4])) // ["piglet", 4]);
+// console.log(diffArray([1, "calf", 3, "piglet"], [7, "filly"]))// === [1, "calf", 3, "piglet", 7, "filly"]);
