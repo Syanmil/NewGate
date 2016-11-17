74e544bf7028c45209c0f5cef295a7c409d86e82
Syanmil <syanmil@yahoo.com>
2016-11-17 14:34:32 +0700

    initialize search and replace quest and with test driven

---
+++ freeCodeCamp/SearchAndReplace.js
@@ -0,0 +1,10 @@
+function myReplace(str, before, after) {
+  return str;
+}
+
+console.log(myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped"));
+console.log(myReplace("Let us go to the store", "store", "mall")); // should return "Let us go to the mall".
+console.log(myReplace("He is Sleeping on the couch", "Sleeping", "sitting")); // "He is Sitting on the couch".
+console.log(myReplace("This has a spellngi error", "spellngi", "spelling")); // "This has a spelling error".
+console.log(myReplace("His name is Tom", "Tom", "john")); //"His name is John".
+console.log(myReplace("Let us get back to more Coding", "Coding", "algorithms")); // "Let us get back to more Algorithms".
