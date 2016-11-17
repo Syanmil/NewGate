bb99db840cfd86eb207d62483168e1260aee40f9
Syanmil <syanmil@yahoo.com>
2016-11-17 14:57:06 +0700

    initialize pig latin with test driven method

---
+++ freeCodeCamp/PigLatin.js
@@ -0,0 +1,11 @@
+function translatePigLatin(str) {
+  return str;
+}
+
+translatePigLatin("consonant");
+
+console.log(translatePigLatin("california")) // "aliforniacay".
+console.log(translatePigLatin("paragraphs")) // "aragraphspay".
+console.log(translatePigLatin("glove")) // "oveglay".
+console.log(translatePigLatin("algorithm")) // "algorithmway".
+console.log(translatePigLatin("eight")) // "eightway". 
