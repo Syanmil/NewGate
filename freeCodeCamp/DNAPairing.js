de488e3833dcd119369da3b75daf87d7258b5b69
Syanmil <syanmil@yahoo.com>
2016-11-17 15:30:49 +0700

    initializing pairElement DNA (base pair) with 2 test must be passed

---
+++ freeCodeCamp/DNAPairing.js
@@ -0,0 +1,9 @@
+
+function pairElement(str) {
+  return str;
+}
+
+pairElement("GCG");
+
+console.log(pairElement("ATCGA")); //[["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]].
+console.log(pairElement("TTGAG")); // [["T","A"],["T","A"],["G","C"],["A","T"],["G","C"]]. 
