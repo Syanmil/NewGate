1b239ee4e42a80da9bcc74fa6b5ee81d4e1afb69
Syanmil <syanmil@yahoo.com>
2016-11-17 15:11:37 +0700

    finishing if the first character is vowel

--- freeCodeCamp/PigLatin.js
+++ freeCodeCamp/PigLatin.js
@@ -1,11 +1,20 @@
 function translatePigLatin(str) {
+  var vowel = ["a", "i", "u", "e", "o"];
+  for (var i = 0; i < vowel.length; i++){
+    if (str[0] == vowel[i]){
+      return str += "way"
+    }
+  }
+  for (var j = 0; j < str.length; j++){
+    
+  }
   return str;
 }
 
-translatePigLatin("consonant");
+console.log(translatePigLatin("consonant"));
 
 console.log(translatePigLatin("california")) // "aliforniacay".
 console.log(translatePigLatin("paragraphs")) // "aragraphspay".
 console.log(translatePigLatin("glove")) // "oveglay".
 console.log(translatePigLatin("algorithm")) // "algorithmway".
-console.log(translatePigLatin("eight")) // "eightway". 
+console.log(translatePigLatin("eight")) // "eightway".
