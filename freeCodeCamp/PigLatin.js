75a24e2e2a24249f7278c0cba60815e15c31c1cb
Syanmil <syanmil@yahoo.com>
2016-11-17 15:24:58 +0700

    finish yeay, still can be refactored for front end directly used

--- freeCodeCamp/PigLatin.js
+++ freeCodeCamp/PigLatin.js
@@ -1,18 +1,20 @@
 function translatePigLatin(str) {
   var vowel = ["a", "i", "u", "e", "o"];
-  for (var i = 0; i < vowel.length; i++){
-    if (str[0] == vowel[i]){
-      return str += "way"
+  for (var j = 0; j < str.length; j++){
+    for (var i = 0; i < vowel.length; i++){
+      if (str[0] == vowel[i]){
+        return str += "way";
+      } else if (str[j] == vowel[i]){
+        var index = str.indexOf(str[j]);
+        var front = str.substr(index);
+        var end = str.substr(0, index);
+        return front + end + "ay"
+      }
     }
   }
-  for (var j = 0; j < str.length; j++){
-    
-  }
-  return str;
 }
 
 console.log(translatePigLatin("consonant"));
-
 console.log(translatePigLatin("california")) // "aliforniacay".
 console.log(translatePigLatin("paragraphs")) // "aragraphspay".
 console.log(translatePigLatin("glove")) // "oveglay".
