3b195b03322f677920a6672e3b5bb71bc6f5679e
Syanmil <syanmil@yahoo.com>
2016-11-21 10:19:21 +0700

    done?

--- freeCodeCamp/BooWho.js
+++ freeCodeCamp/BooWho.js
@@ -1,6 +1,10 @@
 function booWho(bool) {
   // What is the new fad diet for ghost developers? The Boolean.
-  return bool;
+  if (bool === true || bool === false){
+    return true
+  } else {
+    return false
+  }
 }
 
 console.log(booWho(true)) // true.
@@ -12,4 +16,4 @@ console.log(booWho(1)) // false.
 console.log(booWho(NaN)) // false.
 console.log(booWho("a")) // false.
 console.log(booWho("true")) // false.
-console.log(booWho("false")) // false. 
+console.log(booWho("false")) // false.
