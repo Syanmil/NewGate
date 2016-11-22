3c3465ce04c1725f5243c98128019d8cfe1d5dab
Syanmil <syanmil@yahoo.com>
2016-11-22 13:28:36 +0700

    done with more brute force

--- freeCodeCamp/smallestCommon.js
+++ freeCodeCamp/smallestCommon.js
@@ -1,10 +1,30 @@
 function smallestCommons(arr) {
   var max = Math.max(...arr)
   var min = Math.min(...arr)
+  var maxer = max*max*max*max
   for (var i=min+1; i<max; i++){
     arr.push(i)
   }
-  return arr;
+  arr.sort(function(a,b){
+    if(a>b){
+      return 1
+    } else if (a<b){
+      return -1
+    } else {
+      return 0
+    }
+  })
+  var boo = max
+  while (true){
+    var fuss = arr.every(function(elem, index, array){
+      return boo%elem == 0;
+    });
+    if (fuss === false){
+      boo += max;
+    } else {
+      return boo
+    }
+  }
 }
 
 
