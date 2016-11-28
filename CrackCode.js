b4ec9d301b677de567514bb9f6ce0b9d31545bcd
Syanmil <syanmil@yahoo.com>
2016-11-28 10:58:10 +0700

    stuck here

--- CrackCode.js
+++ CrackCode.js
@@ -13,12 +13,28 @@ maka kode tidak valid.
 tersebut dan menemukan nama pelaku dengan cara memecah teks panjang ke beberapa kata,
 
 dan dipisahkan berdasarkan "C", "O", "D", dan "E" yang pertama kali ditemukan.*/
-function analyzeDyingMassage(code){
-  return code
-}
-console.log(analyzeDyingMassage("CHARLESOXDONEULER"));
-console.log(analyzeDyingMassage("CHAMICAELOTNIELDANELLAINE"));
-console.log(analyzeDyingMassage("MARIAHCHARLIEOZIEDELEANOR"));
+var crack = ["C", "O", "D", "E"]
+function analyzeDyingMessage(code){
+  var result = [];
+  var save = -1;
+  for (var i = 0; i < code.length; i++){
+    for (var j = 0; j < crack.length; j++){
+      save += 1
+      if (code.indexOf(crack[j]) == -1){
+        return false
+      }
+      if (save > code.indexOf(crack[j])){
+        return false
+      }
+    }
+  }
+  return true;
+};
+
+console.log(analyzeDyingMessage("CHARLESOXDONEULER")); // "Charles Ox Don Euler"
 console.log(analyzeDyingMessage("CODIMARIJAMO")); // false
 console.log(analyzeDyingMessage("CODE")); // "C O D E"
 console.log(analyzeDyingMessage("CODING")); // false
+console.log(analyzeDyingMessage("CHAMICAELOTNIELDAN")); // false
+console.log(analyzeDyingMessage("CHAMICAELOTNIELDANELLAINE")); // "Chamicael Otniel Dan Ellaine"
+console.log(analyzeDyingMessage("MARIAHCHARLIEOZIEDELEANOR")); // "Mariah Charlie Ozie D Eleanor"
