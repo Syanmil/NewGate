7bae57d9f15293cf3efd0778ef3aa72f982aa460
Syanmil <syanmil@yahoo.com>
2017-04-25 16:10:43 +0700

    done

--- freeCodeCamp/sumAllPrime.js
+++ freeCodeCamp/sumAllPrime.js
@@ -1,6 +1,21 @@
-function sumPrimes(num) {
-
-  return num;
+function sumPrimes (num) {
+  var prime = [2, 3]
+  var newPrime = prime[prime.length - 1]
+  while (num >= newPrime) {
+    var isPrime = true
+    prime.forEach(number => {
+      if (newPrime % number === 0) {
+        isPrime = false
+      }
+    })
+    if (isPrime) {
+      prime.push(newPrime)
+    }
+    newPrime += 2
+  }
+  return prime.reduce((curr, prev) => {
+    return curr + prev
+  })
 }
 
 console.log(sumPrimes(10) === 17)
