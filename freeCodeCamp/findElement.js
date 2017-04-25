395946a31eb95dae9efef82eb723513df261d9ca
Syanmil <syanmil@yahoo.com>
2017-04-25 16:37:04 +0700

    found element

--- freeCodeCamp/findElement.js
+++ freeCodeCamp/findElement.js
@@ -1,8 +1,8 @@
 function findElement (arr, func) {
-  var num = 0
-  return num
+  var found = arr.filter(func)
+  return found.length > 0 ? found[0] : undefined
 }
 
-findElement([1, 2, 3, 4], function (num) { return num % 2 === 0 })
-findElement([1, 3, 5, 8, 9, 10], function (num) { return num % 2 === 0 }) // should return 8.
-findElement([1, 3, 5, 9], function (num) { return num % 2 === 0 }) // should return undefined
+console.log(findElement([1, 2, 3, 4], function (num) { return num % 2 === 0 }))
+console.log(findElement([1, 3, 5, 8, 9, 10], function (num) { return num % 2 === 0 }))
+console.log(findElement([1, 3, 5, 9], function (num) { return num % 2 === 0 }))
