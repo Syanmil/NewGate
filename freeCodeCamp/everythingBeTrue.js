b52a30dea1a55893999b61ffaebfe7a619f725c0
Syanmil <syanmil@yahoo.com>
2017-07-24 14:32:05 +0700

    adding everything be true done

---
+++ freeCodeCamp/everythingBeTrue.js
@@ -0,0 +1,20 @@
+function truthCheck(collection, pre) {
+  let check = true
+  collection.forEach(data => {
+    if (!data[pre]) {
+      check = false
+    }
+  })
+  return check
+}
+
+console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex"));
+// truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex") should return true.
+console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex")) // should return false.
+// truthCheck([{"user": "Tinky-Winky", "sex": "male", "age": 0}, {"user": "Dipsy", "sex": "male", "age": 3}, {"user": "Laa-Laa", "sex": "female", "age": 5}, {"user": "Po", "sex": "female", "age": 4}], "age") should return false.
+// truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true}, {"name": "FastFoward", "onBoat": null}], "onBoat") should return false
+// truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true, "alias": "Repete"}, {"name": "FastFoward", "onBoat": true}], "onBoat") should return true
+// truthCheck([{"single": "yes"}], "single") should return true
+// truthCheck([{"single": ""}, {"single": "double"}], "single") should return false
+// truthCheck([{"single": "double"}, {"single": undefined}], "single") should return false
+// truthCheck([{"single": "double"}, {"single": NaN}], "single") should return false
